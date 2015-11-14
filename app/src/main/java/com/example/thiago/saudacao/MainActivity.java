95d9e3c171421ce3d2aaa18dd791af9734d1ef06
thiagomoraesp@gmail.com <thiagomoraesp@gmail.com>
2015-11-14 12:30:28 -0200

    first commit

---
+++ app/src/main/java/com/example/thiago/saudacao/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.thiago.saudacao;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void saudacao(View view)
+    {
+
+        //pega o valor da caixa de texto
+        TextView texto = (TextView) findViewById(R.id.editText);
+        TextView texto2 = (TextView) findViewById(R.id.editText2);
+
+
+
+        //instancia o toast
+        Context context = getApplicationContext();
+        Resources res = getResources();
+
+
+
+        String text = String.format(res.getString(R.string.texto_principal), texto.getText(),texto2.getText());
+
+
+
+        int duration = Toast.LENGTH_SHORT;
+
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+
+
+
+
+    }
+}
